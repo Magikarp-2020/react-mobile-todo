5e0b3331e2f0b871dda53d3b6d049e0adfb4b461
chao7.li <chao7.li@56qq.com>
2017-12-29 17:33:14 +0800

    my

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import Index from './routes/Index/index';
-import Test from './routes/Test/index';
+import My from './routes/My/index';
 import New from './routes/New/index';
 
 function RouterConfig({ history }) {
@@ -9,7 +9,7 @@ function RouterConfig({ history }) {
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={Index} />
-        <Route path="/test" exact component={Test} />
+        <Route path="/my" exact component={My} />
         <Route path="/new" exact component={New} />
       </Switch>
     </Router>
